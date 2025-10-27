62b4f3d3a5a6d9c88d4434e5732c1d88d92f8e77
ali <ali.hassan.salman.2000@gmail.com>
2025-10-27 10:30:06 +0300

    add routes to the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-
+import NavBar from "./components/NavBar/NavBar.jsx";
+import { Route, Routes } from 'react-router';
+import MailboxList from "./components/MailboxList/MailboxList.jsx";
+import MailboxForm from "./components/MailboxForm/MailboxForm.jsx";
+import MailboxDetails from "./components/MailboxDetails/MailboxDetails.jsx";
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  return (
+    <>
+      <NavBar />
+
+      <Routes>
+        <Route path="/" element={<h1>Post Office</h1>} />
+        <Route path="/mailboxes" element={<MailboxList />} />
+        <Route path="/new-mailbox" element={<MailboxForm />} />
+        <Route path="/mailboxes/:mailboxId" element={<MailboxDetails />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
